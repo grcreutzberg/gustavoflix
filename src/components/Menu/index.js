41a28799ed012c6e52706d0fbabfa40e975499cc
Gustavo Roger Creutzberg <gustavo.creutzberg@gmail.com>
2020-08-06 06:41:52 -0300

    Commit part 04

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,22 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import Button from '../Button';
-import './style.css'
-
+import './style.css';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <a>
-                    <img className="Logo" src={Logo} alt="GustavoFlix Logo" />
-                </a>
-            </Link>
-            <Button as={Link} className="ButtonLink" to="/cadastro/video">
-                Novo vídeo
-            </Button>
-        </nav>
-    );
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <a>
+          <img className="Logo" src={Logo} alt="GustavoFlix Logo" />
+        </a>
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,89 +1,94 @@
 import React, { useState } from 'react';
-import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-    const valoresIniciais = {
-        titulo: '',
-        descricao: '',
-        cor: '#DB202C'
+  const valoresIniciais = {
+    titulo: '',
+    descricao: '',
+    cor: '#DB202C',
 
-    }
-    //const [categorias, setCategorias] = useState([{cor: "#DB202C", descricao: "Uma descrição para a c…", titulo: 'Teste' }]);
-    const [categorias, setCategorias] = useState([]);
-    const [values, setValues] = useState(valoresIniciais);
+  };
+  // const [categorias, setCategorias] = useState([{cor: "#DB202C", descricao: "Uma descrição para a c…", titulo: 'Teste' }]);
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
 
-    function setValue(key, value) {
-        setValues({
-            ...values,
-            [key]: value,
-        });
-    }
+  function setValue(key, value) {
+    setValues({
+      ...values,
+      [key]: value,
+    });
+  }
 
-    function valueChange(event) {
-        //const { getAttribute, value } = event.target;
-        //setValue(getAttribute('name'), value);
-        setValue(
-            event.target.getAttribute('name'),
-            event.target.value
-        );
-    }
-
-    return (
-        <PageDefault>
-            <h1>Cadastro de Categoria: {values.titulo}</h1>
-            <form style={{ background: values.cor }} onSubmit={function handleSubmit(event) {
-                event.preventDefault();
-                setCategorias([
-                    ...categorias,
-                    values
-                ]);
-
-                setValues(valoresIniciais);
-            }}>
-                
-                <FormField 
-                    label='Categoria:'
-                    type='text'
-                    name='titulo'
-                    value={values.titulo}
-                    onChange={valueChange}
-                />
-                <FormField
-                    label='Descrição:'
-                    type='text'
-                    name='descricao'
-                    value={values.descricao}
-                    onChange={valueChange}
-                />
-                <FormField
-                    label='Cor:'
-                    type='color'
-                    name='cor'
-                    value={values.cor}
-                    onChange={valueChange}
-                />
-                <button>
-                    Cadastrar
-                </button>
-            </form>
-
-            <ul>
-                {categorias.map((categoria, indice) => {
-                    return (
-                        <li key={`${categoria}${indice}`}>
-                            {categoria.titulo}
-                        </li>
-                    );
-                })}
-            </ul>
-
-            <Link to="/cadastro/video">
-                Cadastro video
-        </Link>
-        </PageDefault>
+  function valueChange(event) {
+    // const { getAttribute, value } = event.target;
+    // setValue(getAttribute('name'), value);
+    setValue(
+      event.target.getAttribute('name'),
+      event.target.value,
     );
+  }
+
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {values.titulo}
+      </h1>
+      <form
+        //style={{ background: values.cor }}
+        onSubmit={function handleSubmit(event) {
+          event.preventDefault();
+          setCategorias([
+            ...categorias,
+            values,
+          ]);
+
+          setValues(valoresIniciais);
+        }}
+      >
+
+        <FormField
+          label="Categoria:"
+          type="text"
+          name="titulo"
+          value={values.titulo}
+          onChange={valueChange}
+        />
+        <FormField
+          label="Descrição:"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={valueChange}
+        />
+        <FormField
+          label="Cor:"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={valueChange}
+        />
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => (
+          <li key={`${categoria}${indice}`}>
+            {categoria.titulo}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/cadastro/video">
+        Cadastro video
+      </Link>
+    </PageDefault>
+  );
 }
 
-export default CadastroCategoria
+export default CadastroCategoria;
