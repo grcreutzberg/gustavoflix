39b4f3d36fd37a3f39444bdd18d27884916e5760
Gustavo Roger Creutzberg <gustavo.creutzberg@gmail.com>
2020-08-06 18:23:50 -0300

    Update part 04 IV

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -33,7 +33,8 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = window.location.hostname.includes('localhost') ? 'http://localhost:8080/categorias' : 'https://devgustavoflix.herokuapp.com/categorias';
+      //const URL = 'https://devsoutinhoflix.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
