d688dd7bc447ac9c4a5942a1490cb9ce83cd2592
Gustavo Roger Creutzberg <gustavo.creutzberg@gmail.com>
2020-08-07 19:10:07 -0300

    Commit part 05 Home dinamico

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,64 +1,50 @@
-import React, { useEffect } from 'react';
-import dadosIniciais from '../../data/dados_iniciais.json';
-import Menu from '../../components/Menu';
+import React, { useEffect, useState } from 'react';
+//import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
+import PageDefault from '../../components/PageDefault';
 import categoriasRepository from '../../repositories/categorias';
 
 function Home() {
+  const [dados, setDados] = useState([]);
+
   useEffect(() => {
-    //http://localhost:8080/categorias?_embed=videos
-    categoriasRepository.getAllCategoriesWhithMovie().then(CategoriesWhithMovie => {
-      console.log(CategoriesWhithMovie);
+    // http://localhost:8080/categorias?_embed=videos
+    categoriasRepository.getAllCategoriesWhithMovie().then((CategoriesWhithMovie) => {
+      setDados(CategoriesWhithMovie);
     }).catch((err) => {
       console.log(err.message);
     });
-  });
-
+  }, []);
 
   return (
-    <div style={{ background: '#141414' }}>
-      <Menu />
+    <PageDefault paddingAll={0}>
+      {dados.length === 0 && (<div>Loading...</div>)}
+      {dados.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dados[0].videos[0].titulo}
+                url={dados[0].videos[0].url}
+                videoDescription={dados[0].videos[0].description}
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dados[0]}
+              />
+            </div>
+          );
+        }
 
-      <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription="O que é Front-end?"
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[1]}
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[2]}
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[3]}
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[4]}
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[5]}
-      />
-
-      <Footer />
-    </div>
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
+    </PageDefault>
   );
 }
 
